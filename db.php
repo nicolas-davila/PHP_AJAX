505913436f52e61c281ea72677483e2e9a37ea6c
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-09-02 09:56:54 -0300

    Conexão com banco usando pdo

--- db.php
+++ db.php
@@ -1,5 +1,19 @@
 <?php 
 
-   // Mais tarde irei criar banco de dados 
+   // Começando a usar PDO 
+   $host="localhost";
+   $db="preparo_bolo";
+   $user="root";
+   $pass= "012220";
+
+   try {
+      $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      // echo "Conexão Realizada com Sucesso!";
+
+   } catch (Exception $e) {
+      echo "Erro de conexão: " . $e->getMessage();
+   };
 
 ?>
