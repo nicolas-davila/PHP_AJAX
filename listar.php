c2c6cddbf2fda5a1c29b7bbf63e306269a480729
Gabriel <gabrieloliveira311@outlook.com>
2025-09-10 11:33:07 -0300

    Acertando erro na consulta do sql

--- listar.php
+++ listar.php
@@ -2,10 +2,11 @@
 
     include "db.php";
 
-    $sql = "SELECT * FROM atividades";
+    $sql = "SELECT a.id, a.atividade, a.producao, u.nome AS usuario FROM atividades
+    AS a LEFT JOIN usuarios AS u ON a.usuario_atribuido = u.id";
     $result = $conn->query($sql);
 
-    if ($result->num_rows>0) {
+    if ($result->num_rows > 0) {
         echo "<table border='1' cellpadding='5'>";
         echo "<tr>
                     <th>Id</th>
@@ -20,9 +21,24 @@
             echo "<tr>";
             echo "<td>".$row['id']."</td>";
             echo "<td>".$row['atividade']."</td>";
-            echo "<td>".$row['usuario_atribuido']."</td>";
+            echo "<td>".$row['usuario']."</td>";
             echo "<td>".$row['producao']."</td>";
-            echo "<td>".$row['insumos']."</td>";
+
+            $insumos_query = "SELECT i.descricao_insumos, ai.informacao_insumos 
+            FROM atividade_insumos AS ai JOIN insumos AS i ON i.id = ai.insumos_id
+            WHERE ai.atividade_id = " . $row['id'];
+
+            $insumos_result = $conn->query($insumos_query);
+
+            echo "<td>";
+            
+                if($insumos_result->num_rows > 0) {
+                    while($insumo = $insumos_result->fetch_assoc()) {
+                        echo $insumo['descricao_insumos'] . " - " .$insumo['informacao_insumos'] . "<br>";
+                    };
+                };
+
+            echo"</td>";
             echo "<td>
                     <button class='atribuirInsumos' onclick='atribuirInsumos(this)' data-id='" . $row['id'] . "'>Atribuir Insumos</button>
                     <button class='editar' onclick='editarAtividade(this)' data-id='" . $row['id'] . "'>Editar</button>
