0ff32fba1199080f008e312d77ccbd5b06e73f0d
nicolas-davila <nicolasdavilafsantos@gmail.com>
2025-09-10 13:31:42 -0300

    Trazendo tabela de usuários

---
+++ listar_usuarios.php
@@ -0,0 +1,30 @@
+<?php
+
+    include "db.php";
+
+    $sql = "SELECT * FROM usuarios";
+
+    $result = $conn->query($sql);
+
+    if($result->num_rows>0) {
+        echo "<table border='1' cellpadding='5'>";
+
+        echo "<tr>";
+        echo "<th>Id</th>";
+        echo "<th>Nome</th>";
+        echo "<th>Ações</th>";
+        echo "</tr>";
+
+        while($row = $result->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td> 
+                    <button class='excluirUsuario' onclick='' data-id='" . $row['id'] . "'>Excluir</button>
+                 </td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+
+?>
